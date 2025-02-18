6590c0c08a69c5dd72bf71ca6fdee0bc0a146f7e
weijiajia <123456@qq.com>
2025-02-18 18:35:22 +0800

    refactor(config): 修改 forage配置项
    - 移除了 forage 配置文件中的多余空行
    - 将 'cookie_file' 配置项的默认值从 env('FORAGE_COOKIE_FILE','') 改为 ''

--- config/forage.php
+++ config/forage.php
@@ -1,8 +1,7 @@
 <?php
 
 return [
-
     'account' => env('FORAGE_ACCOUNT', ''),
     'password' => env('FORAGE_PASSWORD', ''),
-    'cookie_file' => env('FORAGE_COOKIE_FILE',''),
+    'cookie_file' => '',
 ];
