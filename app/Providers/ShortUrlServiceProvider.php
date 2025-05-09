8517e4dab165b5c638ac001f356b0a3bef069ce7
weijiajia <123456@qq.com>
2025-05-09 20:16:33 +0800

    feat(provider): 更新 ShortUrlServiceProvider 类，移除 DeferrableProvider 接口并调整方法
    - 将 register 方法改为 boot 方法
    - 优化 ShortUrlService 的实例化逻辑，增加异常处理以确保账号和密码的有效性
    - 移除不再需要的 provides 方法

--- app/Providers/ShortUrlServiceProvider.php
+++ app/Providers/ShortUrlServiceProvider.php
@@ -3,32 +3,27 @@
 namespace App\Providers;
 
 use App\Services\ShortUrlService;
-use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 
-class ShortUrlServiceProvider extends ServiceProvider implements DeferrableProvider
+class ShortUrlServiceProvider extends ServiceProvider
 {
-    public function register(): void
+    public function boot(): void
     {
         $this->app->singleton(ShortUrlService::class, function ($app) {
 
             $account = config('short-url.username');
+            $password = config('short-url.password');
+            $cookieFile = config('short-url.cookie_file', storage_path("app/public/short-url-{$account}.json"));
+
+            if (!$account || !$password) {
+                throw new \Exception('Short URL account or password is not set');
+            }
 
             return new ShortUrlService(
-                account: config('short-url.username'),
-                password: config('short-url.password'),
-                cookieFile: config('short-url.cookie_file', storage_path("app/public/short-url-{$account}.json"))
+                account: $account,
+                password: $password,
+                cookieFile: $cookieFile
             );
         });
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array<int, string>
-     */
-    public function provides(): array
-    {
-        return [ShortUrlService::class];
     }
 } 
